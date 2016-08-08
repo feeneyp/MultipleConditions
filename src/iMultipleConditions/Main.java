c37968f60bf1bd6489bcb743e930e987675ea8f7
feeneyp <feeneyp@patricks-mbp.lan>
2016-08-08 07:43:37 +0200

    added else if's

--- src/iMultipleConditions/Main.java
+++ src/iMultipleConditions/Main.java
@@ -4,23 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		int age = 17;
-		String gender = "male";
+		String gender = "female";
 		boolean young = age < 18;
 		if (young && gender.equalsIgnoreCase("male"))  {
 			System.out.println("boy");
 		}
-		else {
-			if (young) {  //must then be female
-				System.out.println("girl");
-			}
-			else { //not young so must be man or woman
-				if (gender.equalsIgnoreCase("male")) {
-				System.out.println("man");
-				}
-				else {
-				System.out.println("woman");  // female 
-				}
-			}
+		else if (young) { //must then be female
+			System.out.println("girl");
 		}
-	}
+		else if (gender.equalsIgnoreCase("male")) { //not young so must be man
+			System.out.println("man");
+		}
+		else {  // not young, not male so must be woman
+			System.out.println("woman");   
+				}
+		}
 }
