2948597488621aae1e4867a7c480fc692d1e23e8
feeneyp <feeneyp@patricks-mbp.lan>
2016-08-08 07:33:53 +0200

    added boolean variable young

--- src/iMultipleConditions/Main.java
+++ src/iMultipleConditions/Main.java
@@ -5,15 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		int age = 17;
 		String gender = "male";
-		if (age<18 && gender.equalsIgnoreCase("male"))  {
+		boolean young = age < 18;
+		if (young && gender.equalsIgnoreCase("male"))  {
 			System.out.println("boy");
 		}
 		else {
-			if (age<18) {  //clearly <18 and female
+			if (young) {  //must then be female
 				System.out.println("girl");
 			}
-			else { // >=18  
-				if (gender.equalsIgnoreCase("male")) {  // >18 and male
+			else { //not young so must be man or woman
+				if (gender.equalsIgnoreCase("male")) {
 				System.out.println("man");
 				}
 				else {
